984a386a3163870bccbe89b1ff9730474f7e9c48
Ulises Santana <ulisesantana@gmail.com>
2023-04-23 13:57:04 +0100

    ✨ add working version

--- README.md
+++ README.md
@@ -156,3 +156,10 @@ See code
 
 <!-- commandsstop -->
 
+Add to your .zshenv:
+```env
+export TOGGL_API_TOKEN="email:password"
+export TOGGL_WORKSPACE_ID=1234567890
+export TOGGL_DEFAULT_TIME_ENTRY="Doing stuff"
+export TOGGL_DEFAULT_PROJECT=1234567890
+```
---
+++ tests/test_week.py
@@ -0,0 +1,29 @@
+import datetime
+from track import week_helper
+
+def test_get_week_dates():
+    monday = datetime.date(2023, 4, 24)
+    sunday = datetime.date(2023, 4, 30)
+    # Test case 1: Monday
+    date1 = monday
+    start1, end1 = week_helper.get_week_dates(date1)
+    assert start1 == monday
+    assert end1 == sunday
+
+    # Test case 2: Wednesday
+    date2 = datetime.date(2023, 4, 26)
+    start2, end2 = week_helper.get_week_dates(date2)
+    assert start2 == monday
+    assert end2 == sunday
+
+    # Test case 3: Sunday
+    date3 = sunday
+    start3, end3 = week_helper.get_week_dates(date3)
+    assert start3 == monday
+    assert end3 == sunday
+
+    # Test case 4: New Year (January 1st)
+    date4 = datetime.date(2023, 1, 1)
+    start4, end4 = week_helper.get_week_dates(date4)
+    assert start4 == datetime.date(2022, 12, 26)
+    assert end4 == datetime.date(2023, 1, 1)
