ad879909f7824f449e61b41d0f47f1ca4a7f0bb6
Ulises Santana <ulisesantana@gmail.com>
2023-04-26 22:06:06 +0100

    ✅ add test for today command

--- track/cases/__init__.py
+++ track/cases/__init__.py
@@ -1,4 +1,5 @@
 from .continue_with_last_time_entry_use_case import ContinueWithLastTimeEntryUseCase
+from .get_today_report_use_case import GetTodayReportUseCase
 from .get_current_time_entry_use_case import GetCurrentTimeEntryUseCase
 from .start_time_entry_use_case import StartTimeEntryUseCase
 from .stop_time_entry_use_case import StopTimeEntryUseCase
