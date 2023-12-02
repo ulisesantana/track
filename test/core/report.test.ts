d2f64a2197a7b6ce5a20a24bae343f0a9a1d76bb
Ulises Santana <ulisesantana@gmail.com>
2023-12-02 17:38:45 +0000

    🙈 ignore coverage folderˆ

--- test/core/report.test.ts
+++ test/core/report.test.ts
@@ -11,9 +11,8 @@ describe('Report', () => {
     ]);
     const projects = { '1': new Project(1, 'Project A'), '2': new Project(2, 'Project B') };
     const report = new Report(timeEntries, projects);
-    const reportString = report.toString();
 
-    expect(reportString).to.be.equal(`01h 28m 03s
+    expect(report.toString()).to.be.equal(`01h 28m 03s
   - 01h 00m 00s - Work (Project A)
   - 00h 28m 03s - Meeting (Project B)`);
   });
